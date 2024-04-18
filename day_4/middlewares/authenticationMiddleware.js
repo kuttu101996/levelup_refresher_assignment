09e72c65ad47de2f2452461dd6018789ac2b4a84
tojoy123 <mail.joy@yahoo.com>
2024-04-18 22:37:58 +0530

    day_2 day_4

---
+++ day_4/middlewares/authenticationMiddleware.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token)
+    return res.status(401).json({ message: "Authentication token required" });
+
+  jwt.verify(token, process.env.secret_key, (err, decoded) => {
+    if (err) return res.status(403).json({ message: "Invalid token" });
+    req.userId = decoded.userID;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
